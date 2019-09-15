eea54acf2fde880e8894718f1324b6b0568906d3
Aldert Vaandering <paperfeed@gmail.com>
2019-09-15 14:08:50 +0430

    env value for netlify deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
+    publicPath: process.env.NODE_ENV === 'production' && process.env.netlify !== true
         ? '/aboutblank/'
         : '/',
     pluginOptions: {
